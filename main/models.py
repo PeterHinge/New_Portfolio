912758c528613d295aa992b2fc9c59bfe12f7156
unknown <contactpeterhinge@gmail.com>
2019-08-15 16:15:44 +0200

    added visuals

--- main/models.py
+++ main/models.py
@@ -4,12 +4,13 @@ from datetime import datetime
 
 # Create your models here.
 class Project(models.Model):
-    project_title = models.CharField(max_length=200)
+    project_title = models.CharField(max_length=100)
     project_published = models.DateTimeField('date published', default=datetime.now())
+    project_description = models.CharField(max_length=300)
     project_content = models.TextField()
-    project_slug = models.CharField(max_length=200)
-    project_github_url = models.CharField(max_length=200)
-    project_thumbnail = models.CharField(max_length=200, default="no_image")
+    project_slug = models.CharField(max_length=100)
+    project_github_url = models.CharField(max_length=100)
+    project_thumbnail = models.CharField(max_length=100, default="no_image")
 
     def __str__(self):
         return self.project_title
