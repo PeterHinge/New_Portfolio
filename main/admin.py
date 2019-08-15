bb042e1abac72c2df49dabc59aeafc0bed900e6d
unknown <contactpeterhinge@gmail.com>
2019-08-15 19:37:52 +0200

    ready for deployment

--- main/admin.py
+++ main/admin.py
@@ -6,7 +6,7 @@ from .models import Project
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Title/date", {'fields': ['project_title', 'project_published']}),
-        ("Content", {'fields': ['project_content', 'project_thumbnail']}),
+        ("Content", {'fields': ['project_description', 'project_content', 'project_thumbnail']}),
         ("URLs", {'fields': ['project_github_url', 'project_slug']})
     ]
 
